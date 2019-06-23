ccadf4d2fb0e37746d58334045b454b5d810b236
Lee Biddiscombem <lee.biddiscombe@btinternet.com>
2019-06-23 20:06:05 +0100

    removed withInlineEdit HOC / hook

--- src/hooks/useTaskStore.js
+++
@@ -1,6 +0,0 @@
-import { createContext, useContext } from 'react'
-import TaskStore from '../stores/TaskStore'
-
-const storeContext = createContext(new TaskStore())
-
-export const useTaskStore = () => useContext(storeContext)
